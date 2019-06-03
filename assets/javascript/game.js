e96f48e7bee6001cfc236487af38b4190c275be5
Rina Razumov <rinash3@gmail.com>
2019-06-02 23:13:19 -0400

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,105 @@
-//press any key to start
-//define variables for selectedWord, userGuess,Wins,Number of Guesses Remaining,Letters Already Guessed
-//var selectedWord; // Selected word
-var userGuess; // Geusses
-var geusses = []; // Stored geusses
-var guessesLeft; // Number of Guesses Remaining
-var answerArray = [];
+//Counters:
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+//Empty variables to store values
+var selectedWord = "";
+var lettersInWord = [];
+var numBlanks = 0; //spaces based on selected word
+var spaceSuccess = []; //array to hold spaces and guessed letters
+var guessesSoFar = []; // stores wrong guesses
+
 
 //define list of available words to pick from
-var wordsToSelect = ["zebra", "lion", "hipo", "panda", "gorilla", "giraffe", "elephant", "tiger", "monkey", "bear"];
+var words = ["zebra", "lion", "hipo", "panda", "gorilla", "giraffe", "elephant", "tiger", "monkey", "bear"];
 //pick a random word from wordsToSelect
-var selectedWord = wordsToSelect[Math.floor(Math.random() * wordsToSelect.length)];
-console.log(selectedWord);
-//display the number of letters in the word _ _ _ _ _
 
-var wordArray = [];
-for (var i = 0; i < selectedWord.length; i++) {
-    answerArray[i] = "_";
-    console.log(answerArray);
+
+
+function Game() {
+    //computer generates random word from words array
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    // split the individual word into separate arrays, and store in new array 
+    lettersInWord = selectedWord.split("");
+
+    //store length of word in blanks, for later use
+    numBlanks = lettersInWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < numBlanks; i++) {
+        spaceSuccess.push("_");
+    }
+
+    //showing the "_" within HTML
+    // document.getElementById("selectedWord").innerHTML = "  " + numBlanks.join("  ");
+
+    //console logging 
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numBlanks);
+    console.log(spaceSuccess);
 }
-var remainingLetters = selectedWord.length;
-while (remainingLetters > 0) {
 
-    for (var j = 0; j < selectedWord.length; j++) {
-        if (selectedWord[j] === userGuess) {
-            answerArray[j] = userGuess;
-            remainingLetters--;
-            // console.log(answerArray);
+function reset() {
+    guessesLeft = 10;
+    guessesSoFar = [];
+    spaceSuccess = [];
+    Game()
+}
+
+function checkLetters(letter) {
+    var letterInWord = false;
+    //if the generated word is equal to the letter entered... then variable is true
+    for (var i = 0; i < numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+            letterInWord = true;
         }
     }
 
+    if (letterInWord) {
+        //check each letter to see if it matches word
+        for (var i = 0; i < numBlanks; i++) {
+            if (selectedWord[i] == letter) {
+                spaceSuccess[i] = letter;
+            }
+        }
+    } else {
+        guessesSoFar.push(letter);
+        guessesLeft--;
+    }
+    console.log(spaceSuccess);
 }
 
+function endOfGame() {
+    if (lettersInWord.toString() == spaceSuccess.toString()) {
+        wins++;
+        reset()
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
 
-//listen to the users clicks
-//if user guessed correct letter
-// find the letter placement in the word
-//place the letter in the correct place
-//number of available gueses is not changed
-//update leletters guessed array
-//if user guess is not in the word 
-//reduce number of guesses remaining by 1
-//update letters guessed array
-//if user guessed the word
-// increase wins by 1
-//pick the next word
-//if number of guesses remaining is smaller than 1
-//pick the next random word
-//display the number of letters in the new word
-//losses increased by 1
+    } else if (guessesLeft === 0) {
+        losses++;
+        reset()
+
+        document.getElementById('losses').innerHTML = "Losses: " + losses;
+    }
+    //display counters
+    document.getElementById("selectedWord").innerHTML = "  " + spaceSuccess.join(" ");
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left:" + guessesLeft;
+}
+
+Game()
+
+//check for keyup, and convert to lowercase then store in guesses
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    //check to see if guess entered matches value of selected word
+    checkLetters(userGuess);
+
+    console.log(userGuess);
+    endOfGame();
+
+
+    document.getElementById('guessesSoFar').innerHTML = "Guesses So Far: " + guessesSoFar;
+}
